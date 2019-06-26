084eb505b7d798839b821f52d2b5a31ba0c73647
eduardo <xmartinez@stuba.sk>
2019-06-26 22:59:45 +0200

    BeforeDirectives

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
+  @ViewChild('nameInput',{static: true}) nameInputRef: ElementRef;
+  @ViewChild('amountInput',{static: true}) amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  onAddItem(){
+    const nameIn = this.nameInputRef.nativeElement.value;
+    const amountIn = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(nameIn,amountIn);
+    this.ingredientAdded.emit(newIngredient);
+  }
+
 
 }
